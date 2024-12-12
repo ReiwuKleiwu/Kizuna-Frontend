ba1ed78e99f1881e8c1d53b7ad3a10c383563d83
Nick Struckmeyer <struckmeyerni@gmail.com>
2024-12-12 12:58:14 +0100

    KIZUNA-FE :: Fixed scrolling behaviour of member settings

--- components/desktop/room-settings/MemberSettings.vue
+++ components/desktop/room-settings/MemberSettings.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="flex flex-1">
-        <div class="flex-1 grid grid-cols-[1fr_min(70ch,_100%)_1fr] gap-2">
+    <div class="flex flex-1 h-full">
+        <div
+            class="flex-1 grid grid-cols-[1fr_min(70ch,_100%)_1fr] gap-2 h-full"
+        >
             <div
-                class="col-span-1 col-start-2 overflow-y-auto overflow-x-hidden"
+                class="col-span-1 col-start-2 overflow-y-auto overflow-x-hidden h-full"
             >
                 <div class="mr-2 flex gap-2 flex-col">
-                    <MemberSettingsUser />
-                    <MemberSettingsUser />
                     <MemberSettingsUser />
                     <MemberSettingsUser />
                     <MemberSettingsUser />

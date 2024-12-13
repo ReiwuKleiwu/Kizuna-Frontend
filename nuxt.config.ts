8b3fa89a225c9a984aa680bd1acf871d24a852ba
Nick Struckmeyer <struckmeyerni@gmail.com>
2024-12-13 14:24:12 +0100

    KIZUNA-FE :: Added Pinia package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
         '@nuxtjs/color-mode',
         '@nuxt/icon',
         '@nuxt/eslint',
+        '@pinia/nuxt',
     ],
     colorMode: {
         classSuffix: '',
